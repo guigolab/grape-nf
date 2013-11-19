08d17e3241fea69cf9435ec125af8eeb476c22c2
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-11-19 17:03:37 +0100

    Install gemtools and flux

--- buildout.sh
+++ buildout.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-logfile="bp.buildout.log"
 python=${1-"python"}
 
 echo "### Blueprint RNAseq pipeline buildout  ###"
@@ -12,6 +11,27 @@ wget -q http://genome.crg.es/~epalumbo/$bintar
 tar xf $bintar
 rm $bintar
 
+# Install GEMtools
+gemtools="GEMTools-static-i3-1.6.2"
+if [ ! -d bin/gemtools-1.6.2-i3 ]; then
+  echo "Install $gemtools"
+  cd bin
+  wget -q http://barnaserver.com/gemtools/releases/$gemtools.tar.gz
+  tar xf $gemtools.tar.gz && rm $gemtools.tar.gz
+  cd ..
+fi
+
+# Install Flux Capacitor
+flux_ver="1.2.4"
+flux="flux-capacitor-$flux_ver"
+if [ ! -d bin/$flux ]; then
+  echo "Install $flux"
+  cd bin
+  wget -q http://sammeth.net/artifactory/barna/barna/barna.capacitor/$flux_ver/$flux.tgz
+  tar xf $flux.tgz && rm $flux.tgz
+  cd ..
+fi
+exit 0
 # set up virtual env
 echo "Set up the Python virtualenv..."
 virtualenv -q --no-site-packages -p $python env
@@ -32,6 +52,7 @@ cd ..
 cd env/lib/python2.7/site-packages/RSeQC-2.3.7-py2.7-linux-x86_64.egg
 mv pysam pysam_old
 ln -s ../pysam
+rm -rf RSeQC-2.3.7
 echo "Deactivate the virtualenv"
 deactivate
 echo "### done ###"
