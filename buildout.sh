b920f54761d9f9b0363b83ac97c658738714bb9c
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-11-19 12:47:20 +0100

    Update buildout

--- buildout.sh
+++ buildout.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 set -e
 
+logfile="bp.buildout.log"
+python=${1-"python"}
+
 # set up binaries
+echo"Downloading binaries..."
 bintar=bp_pipeline_bin.tgz
-wget http://genome.crg.es/~epalumbo/$bintar
+wget -q http://genome.crg.es/~epalumbo/$bintar
 tar xf $bintar
 rm $bintar
 
 # set up virtual env
-virtualenv --no-site-packages env
+echo "Setting up Python virtualenv..."
+virtualenv -q --no-site-packages -p $python env
 . env/bin/activate
-pip install -r requirements.txt
+pip -q install -r pip-requirements.txt
 deactivate
 
 # install RSeQC
-wget http://downloads.sourceforge.net/project/rseqc/RSeQC-2.3.7.tar.gz
+echo "Installing RSeQC..."
+wget i-q http://downloads.sourceforge.net/project/rseqc/RSeQC-2.3.7.tar.gz
 tar xf RSeQC-2.3.7.tar.gz
 rm RSeQC-2.3.7.tar.gz
 cd RSeQC-2.3.7
-python setup.py install
+python setup.py --quiet install
 cd ..
+echo "done"
