d65ab4be9b18a29da1654f66a2a7050e36a914ec
Emilio Palumbo <emiliopalumbo@gmail.com>
2014-12-11 12:02:08 +0100

    Add script for gene quantification

---
+++ bin/TrtoGn_RPKM.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+#
+#  Copyright (c) 2013, Centre for Genomic Regulation (CRG)
+#  Emilio Palumbo, Alessandra Breschi and Sarah Djebali.
+#
+#  This file is part of the Blueprint RNAseq pipeline.
+#
+
+# takes as input a gtf file of transcripts produced by the flux capacitor
+# and makes as output a gene gff file with an rpkm value associated to each gene
+# and computed as the sum of the rpkm of the transcripts belonging to this gene
+# as well as the number of reads supporting the gene computed as the sum of the
+# reads supporting each transcript of this gene.
+# Be careful: makes the following assumptions:
+##############################################
+# 1. the annotation file has gene_id gene as first key/value pair, and transcript_id transcript as second key/value pair
+# 2. the flux output file has transcript_id transcript as first key/value pair
+
+# Programs
+##########
+prog=`basename $0`
+gff2gff="gff2gff.awk"
+
+# Usage
+#######
+function usage {    
+    echo "" >&2
+    echo "Produce a gene gff file with an rpkm value and a number of read value for each gene, corresponding" >&2
+    echo "to the sum of the rpkm and of the number of reads of the transcripts belonging to this gene." >&2
+    echo "" >&2
+    echo "Usage: $prog -a ANNOTATION -i TRANSCRIPTS_GTF -o OUTPUT_FOLDER" >&2
+    echo "" >&2
+    printf "\t-a|--annnotation\t an annotation gtf file\n" >&2
+    printf "\t-i|--input\t\t a flux output file with rpkm value\n" >&2
+    echo "" >&2
+    echo "Options:"
+    echo "" >&2
+    printf "\t-o|--output\t\t the output folder. Default: current folder\n"
+    echo "" >&2
+    exit 1
+}
+
+# Execute getopt
+ARGS=`getopt -o "i:a:o:h" -l "input:,annotation:,output:,help" \
+      -n "$prog" -- "$@"`
+
+#Bad arguments
+if [ $? -ne 0 ];
+then
+  exit 1
+fi
+
+# A little magic
+eval set -- "$ARGS"
+
+output="."
+
+# Now go through all the options
+while true;
+do
+  case "$1" in
+    -i|--input)
+      if [ -n "$2" ];
+      then
+        tr=$2
+      fi
+      shift 2;;
+
+    -a|--annotation)
+      if [ -n "$2" ];
+      then
+        annot=$2
+      fi
+      shift 2;;
+
+    -o|--output)
+      if [ -n "$2" ];
+      then
+        output=$2
+      fi
+      shift 2;;
+    
+    -h|--help)
+      usage
+      shift;;
+
+    --)
+      shift
+      break;;
+  esac
+done
+
+if [ ! $tr ] || [ ! $annot ];then
+    usage
+    exit 1
+fi
+
+annotbase=`basename $annot`
+trbase=`basename $tr`
+withtrlist=${trbase}.${annotbase%.gtf}.gene.withtrlist.gff
+
+echo "I am making the file of genes with associated transcripts from the annotation" >&2
+awk '$3=="transcript"{split($12,a,"\""); trlist[$10]=(trlist[$10])(a[2])(",");}$3=="gene"{line[$10]=$1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9" "$10}END{for(g in line){print line[g], "transcript_ids", "\""trlist[g]"\"\;"}}' $annot | $gff2gff > $output/$withtrlist
+
+echo "I am making the gene file with rpkm and number of reads" >&2
+awk -v fileRef=$tr 'BEGIN{while (getline < fileRef >0){k=9; while(k<=(NF-1)){split($10,a,"\""); if($k=="RPKM"){split($(k+1),b,";"); rpkm[a[2]]=b[1];} if($k=="reads"){split($(k+1),b,";"); reads[a[2]]=b[1];} k+=2}}} {split($12,a,"\""); split(a[2],b,","); s1=0; k=1; while(b[k]!=""){s1+=rpkm[b[k]]; k++} s2=0; k=1; while(b[k]!=""){s2+=reads[b[k]]; k++} print $0, "RPKM", s1"\;", "reads", s2"\;"}' $output/$withtrlist | $gff2gff > $output/${trbase%.gtf}\_gene_with_rpkm.gff
+
+echo "I am removing unuseful files" >&2
+rm $output/$withtrlist
+
+echo "I am done" >&2
+
+
