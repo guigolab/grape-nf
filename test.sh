3b90911bef769477055a5b0023fe06dcdc51ad73
Emilio Palumbo <emiliopalumbo@gmail.com>
2016-03-29 12:02:29 +0200

    Update test.sh script

--- test.sh
+++ test.sh
@@ -4,8 +4,13 @@ set -o pipefail
 
 usage() {
 	echo "Usage:"
-	echo "  $0 <profile> <nextflow_args>"
+	echo "  $0 <profile>"
 	exit 1
+}
+
+mkcd() {
+	local DIR=$1
+	mkdir -p $DIR && cd $DIR
 }
 
 RED="\033[1;31m"
@@ -14,18 +19,18 @@ YELLOW="\033[1;33m"
 BLUE="\033[1;34m"
 NORMAL="\033[0m"
 
-[ $# -eq 0 ] && usage
+[[ $1 == "help" ]] && usage
 
-profile=${1} && shift
-[ -z $profile ] && profile="gemflux"
+PROFILE=${PROFILE-"test"} 
+CHECKDIR=${CHECKDIR-"checksum"}
 
-log_file="test-$profile.log"
+RUN_OPTS="-process.errorStrategy=terminate"
 
-nxf_opts="-process.executor=local -process.errorStrategy=terminate ${@}"
-
-echo -e "==$YELLOW Running pipeline with $BLUE<${profile}>$YELLOW profile$NORMAL"
-nextflow run grape-pipeline.nf -profile $profile --index test-index.txt --genome data/genome.fa --annotation data/annotation.gtf ${nxf_opts} > $log_file
+echo -e "==$YELLOW Running pipeline with profile -> $BLUE${PROFILE}$NORMAL"
+[ ! -x nextflow ] && (curl -fsSL get.nextflow.io | bash && chmod +x nextflow) || true
+./nextflow run . -profile $PROFILE ${RUN_OPTS}
 echo -e "==$YELLOW Compare results$NORMAL"
-dbFile=$(grep "db ->" $log_file | cut -d " " -f5) || (echo -e "==$RED [ERROR] pipeline db file path not found!$NORMAL" >>/dev/stderr; exit 1)
-cat <(cat $dbFile | cut -f3 | xargs md5sum) data/${profile}.md5 | sort | awk '{sums[$1]++; n=split($2,a,"/");files[$1]=a[n]}END{exitStatus=0;for(sum in sums){printf("%-32s\t%2s\n", files[sum], sums[sum]!=2 ? "\033[1;31mKO\033[0m" : "\033[1;32mOK\033[0m"); if(sums[sum]!=2){exitStatus=1}}; exit exitStatus}'
+mkcd $CHECKDIR
+cut -f 3 ../pipeline.db | xargs -I{} ln -fs {}
+md5sum -c ../data/$PROFILE.md5
 echo -e "==$YELLOW DONE$NORMAL"
